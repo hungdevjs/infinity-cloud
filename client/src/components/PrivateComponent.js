e772e88ebcf549615591f4e1dd44f1d78b1d8841
Jack <jack.brightsoft@gmail.com>
2020-07-16 22:50:35 +0700

    refactor sidebar

--- client/src/components/PrivateComponent.js
+++ client/src/components/PrivateComponent.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import history from '../configs/history'
 import Sidebar from './Sidebar'
 export default ({ user, route }) => {
@@ -6,16 +6,10 @@ export default ({ user, route }) => {
         if (!user && route.isLogged) history.push('/login')
     }, [])
 
-    const [sidebarIsHiden, setSidebarIsHiden] = useState(window.innerWidth < 960)
-    window.addEventListener('resize', () => {
-        if (window.innerWidth < 960) setSidebarIsHiden(true)
-        else setSidebarIsHiden(false)
-    })
-
     const Component = route.component
 
-    return <div style={{ paddingLeft: sidebarIsHiden ? 0 : '200px' }}>
+    return <>
         <Sidebar />
         <Component />
-    </div>
+    </>
 }
--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,52 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import Sidebar from "react-sidebar";
+import SidebarContent from "./SidebarContent"
 
-import SidebarContent from './SidebarContent'
+export default () => {
+    const [sidebarOpen, onSetSidebarOpen] = useState(false)
 
-const mql = window.matchMedia(`(min-width: 960px)`);
+    return (
+        <Sidebar
+            sidebar={SidebarContent}
+            open={sidebarOpen}
+            onSetOpen={onSetSidebarOpen}
+            styles={{ sidebar: { background: "white" } }}
+        >
+            <button onClick={() => onSetSidebarOpen(true)}>
+                Open sidebar
+                </button>
+        </Sidebar>
+    );
 
-class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            sidebarDocked: mql.matches,
-            sidebarOpen: false
-        };
-
-        this.mediaQueryChanged = this.mediaQueryChanged.bind(this);
-        this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
-    }
-
-    componentWillMount() {
-        mql.addListener(this.mediaQueryChanged);
-    }
-
-    componentWillUnmount() {
-        this.state.mql.removeListener(this.mediaQueryChanged);
-    }
-
-    onSetSidebarOpen(open) {
-        this.setState({ sidebarOpen: open });
-    }
-
-    mediaQueryChanged() {
-        this.setState({ sidebarDocked: mql.matches, sidebarOpen: false });
-    }
-
-    render() {
-        const { sidebarOpen, sidebarDocked } = this.state
-
-        return (
-            <Sidebar
-                sidebar={SidebarContent}
-                open={this.state.sidebarOpen}
-                docked={this.state.sidebarDocked}
-                onSetOpen={this.onSetSidebarOpen}
-            >
-                {!sidebarOpen && !sidebarDocked && <button onClick={() => this.setState({ sidebarDocked: mql.matches, sidebarOpen: true })}>open</button>}
-            </Sidebar>
-        );
-    }
 }
-
-export default App;
