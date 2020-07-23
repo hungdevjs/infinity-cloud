cc92190933b17edbe88aa581fb91bdf15dd0c95b
Jack <jack.brightsoft@gmail.com>
2020-07-23 22:27:15 +0700

    complete login

--- client/src/redux/action.js
+++ client/src/redux/action.js
@@ -1,4 +1,6 @@
 import { login } from "../utils/api"
+import noti from "../utils/noti"
+import history from "../configs/history"
 
 export const setLoading = status => ({ type: "SET_LOADING", payload: status })
 
@@ -7,9 +9,31 @@ export const userLogin = data => async dispatch => {
 
     try {
         const res = await login(data)
-        console.log(res.data)
-    } catch {
+        if (!res.data || !res.data.status) {
+            throw new Error("Log in failed")
+        }
 
+        const accessToken = res.data.accessToken
+        localStorage.setItem("accessToken", accessToken)
+
+        dispatch({
+            type: "SET_USER",
+            payload: data.email
+        })
+
+        noti({
+            title: "Success",
+            type: "success",
+            message: "Log in successfully"
+        })
+
+        history.push("/")
+    } catch (err) {
+        noti({
+            title: "Failed",
+            type: "danger",
+            message: err.message
+        })
     }
 
     dispatch(setLoading(false))
